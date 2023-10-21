889678c9c642c7fd6c781a5738cc11fd6ae9e8b5
medamshiva20 <65282431+medamshiva20@users.noreply.github.com>
2023-10-21 20:56:16 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,8 +11,6 @@ terraform {
     key = "catalogue-dev"
     region = "us-east-1"
     dynamodb_table = "roboshop-remote-lock"
-    # access_key = "AKIASGS2UUI55EU5ZLNM" 
-    # secret_key = "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"
   }
 }
 
